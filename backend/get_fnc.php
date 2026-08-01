419a8998e20af226993b118b34d1acc69627898b
ruzenathegoat <naufalrahaman013@gmail.com>
2026-08-02 03:38:26 +0700

    Remove legacy scraper and reset scripts

--- backend/get_fnc.php
+++
@@ -1,3 +0,0 @@
-<?php
-$teams = DB::table("teams")->select("name", "logo_url")->whereRaw("LOWER(name) LIKE '%fnatic%'")->get();
-echo json_encode($teams, JSON_PRETTY_PRINT);
--- backend/get_logos.php
+++
@@ -1,3 +0,0 @@
-<?php
-$teams = DB::table("teams")->select("name", "logo_url")->whereIn("name", ["Sentinels", "Paper Rex", "LOUD", "Fnatic", "Natus Vincere", "Gen.G Esports", "DRX", "T1"])->get();
-echo json_encode($teams, JSON_PRETTY_PRINT);
