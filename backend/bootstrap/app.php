ae97c4963d392e5706e527fdf05edc350a6e6a96
ruzenathegoat <naufalrahaman013@gmail.com>
2026-08-11 23:22:41 +0700

    fix scraper

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -28,4 +28,11 @@ return Application::configure(basePath: dirname(__DIR__))
         $exceptions->shouldRenderJsonWhen(
             fn (Request $request) => $request->is('api/*'),
         );
+        $exceptions->render(function (\Illuminate\Auth\AuthenticationException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => $e->getMessage(),
+                ], 401);
+            }
+        });
     })->create();
